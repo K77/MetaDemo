4ade24861d67c87590cbe60deb682a6abce29e7c
K <>
2022-03-02 20:45:21 +0800

    删除之前的study

---
+++ Assets/_Demos/ARFace/FaceBlendShape.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceBlendShape : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mesh mesh = new Mesh();
+        // mesh.AddBlendShapeFrame();
+        mesh.ClearBlendShapes();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
